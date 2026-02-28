9019697e92f83ce02496d51eb46a53668e8a05eb
GPBMAN <158358188+GPBMAN@users.noreply.github.com>
2026-02-28 11:04:45 -0600

    v3
    bigger gaps and faster animations

--- hypr/wallpaper-menu.sh
+++ hypr/wallpaper-menu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-choice=$(printf "Frieren\nGoku" | rofi -dmenu -p "Wallpaper Menu")
+choice=$(printf "Frieren\nGoku\nRain" | rofi -dmenu -p "Wallpaper Menu")
 
 case $choice in
     "Frieren") killall hyprpaper && hyprpaper -c /home/Brob/.config/hypr/Frieren-Wallpaper.conf ;;
     "Goku") killall hyprpaper && hyprpaper -c /home/Brob/.config/hypr/Goku-Wallpaper.conf ;;
+    "Rain") killall hyprpaper && hyprpaper -c /home/Brob/.config/hypr/Rain-Wallpaper.conf ;;
 esac
